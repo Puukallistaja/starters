b42e1e9a50855fe0c1f7ba19f7c562570aa54cfc
Arto Haponen <arto.haponen@unity.pl>
2020-02-01 23:08:57 +0100

    Fix: Integration test

--- client/test/cypress/integration/home/init.spec.js
+++ client/test/cypress/integration/home/init.spec.js
@@ -5,7 +5,7 @@ describe('Landing', () => {
     cy.visit('/')
   })
   it('.should() - assert that <title> is correct', () => {
-    cy.title().should('include', 'Quasar')
+    cy.title().should('include', 'Turakas')
   })
 })
 
