1910b509e390e417ac4376fdffdfcc545aeb6ddb
Arto Haponen <arto.haponen@unity.pl>
2020-02-07 22:56:36 +0100

    Add: Layout/PlainContainer

---
+++ client/src/layouts/PlainContainer.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+q-layout
+  q-page-container
+    router-view
+</template>
+
+<script>
+export default {
+  name: "PlainContainer",
+  data() {
+    return {}
+  },
+}
+</script>
