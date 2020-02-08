c29c82475c38e68775abe4a2422b97a5bcac249f
Arto Haponen <arto.haponen@unity.pl>
2020-02-08 13:09:36 +0100

    Add: Turakas to PlainContainer

---
+++ client/src/components/TheTurakas.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  q-page-sticky(position="center")
+    q-fab(
+      color="primary"
+      size="xl"
+      icon="add"
+      aria-label="Show actions"
+      round
+    )
+</template>
+
+<script>
+export default {
+  name: 'TheTurakas',
+  data () {
+    return {}
+  }
+}
+</script>
--- client/src/layouts/PlainContainer.vue
+++ client/src/layouts/PlainContainer.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
-q-layout
-  q-page-container
-    router-view
+  q-layout
+
+    q-page-container
+      router-view
+      the-turakas
 </template>
 
 <script>
+import TheTurakas from "components/TheTurakas"
 export default {
   name: "PlainContainer",
+  components: {
+    TheTurakas,
+  },
   data() {
     return {}
   },
