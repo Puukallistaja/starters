8a673991eeb0d4ed408a76472e6f4d1590d1c15f
Arto Haponen <arto.haponen@unitygroup.com>
2020-05-11 21:13:25 +0200

    Add: TS

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -6,6 +6,7 @@ module.exports = function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
+    supportTS: true,
     boot: [
       'i18n',
       'axios'
